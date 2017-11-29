c4ec10263e3f65c4a91c94c477d48661030912ed
vittoriop77 <vittorio.pavesi@gmail.com>
2017-11-29 18:55:29 +0100

    Changed BallonIcon

--- CheckConnection.ps1
+++ CheckConnection.ps1
@@ -1,7 +1,12 @@
+# *************************************************************************************************
+# Notify if target is not reachable (I use it to check my connection when I travel)
+# To launch with hidden windows use: Powershell.exe -windowstyle hidden -File CheckConnection.ps1
+# *************************************************************************************************
+
 param
 (
     $target = "8.8.8.8",
-    $retryTime = 30,
+    $retryTime = 10,
     $msg = "Connection error to $target ("+(Get-Date -Format HH:mm)+")",
     $title = "Warning",
     $icon = "None",
@@ -10,6 +15,8 @@ param
 [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | out-null
 [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") | out-null
 
+$Balloon = new-object System.Windows.Forms.NotifyIcon
+$Balloon.Visible = $true;
 
 while ($true) {
     #Test-Connection -ComputerName "Server01" -Count 3 -Delay 2 -TTL 255 -BufferSize 256 -ThrottleLimit 32
@@ -17,15 +24,16 @@ while ($true) {
     # Use an If statement to determine if the machine responded or not and output accordingly
     If ($pinged) 
         { 
+			$Balloon.Icon = [System.Drawing.SystemIcons]::Information
             Write-Host "OK" 
         }
 
     Else { 
         Write-Host "No Reply" 
-        $Balloon = new-object System.Windows.Forms.NotifyIcon
-        $Balloon.Icon = [System.Drawing.SystemIcons]::Information
-        $Balloon.Visible = $true;
+		$Balloon.Icon = [System.Drawing.SystemIcons]::Warning
         $Balloon.ShowBalloonTip($timeout, $title, $msg, $icon);
     }
     sleep $retryTime
 }
+
+
